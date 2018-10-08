208d47284d305f0c3d559c0e17dfcbeb258bf5e8
mguerrea <mguerrea@student.42.fr>
2018-10-08 20:34:42 +0100

    fonctions de reperage sur la grille

---
+++ pos.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pos.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguerrea <mguerrea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 20:23:18 by mguerrea          #+#    #+#             */
+/*   Updated: 2018/10/08 20:34:21 by mguerrea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_pos *position(int index, int width)
+{
+    t_pos *pos;
+
+    pos->x = index % (width + 1);
+    pos->y = index / (width + 1);
+    return (pos);
+}
+
+char *get_char_at(t_pos *pos, char *grid, int width)
+{
+    int index;
+
+    index = pos->y * (width + 1) + pos->x;
+    return (&grid[index]);
+}
