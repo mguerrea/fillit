b6258b39c57c2321ab85cf8b4df2a9d487ded7fb
Lucas B <lbenard@student.42.fr>
2018-10-09 00:03:39 +0200

    it works!

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/08 21:47:26 by lbenard           #+#    #+#             */
-/*   Updated: 2018/10/08 23:31:12 by lbenard          ###   ########.fr       */
+/*   Updated: 2018/10/09 00:02:43 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,46 @@
 #include "pos.h"
 #include <stdio.h>
 
+int	parse_one_tetrimino(int fd, t_tetrimino **tetriminos, ssize_t nb)
+{
+	t_pos	origin;
+	t_pos	temp;
+	char	*str;
+	ssize_t	j;
+	ssize_t	k;
+
+	j = -1;
+	origin.x = -1;
+	while (++j < 4)
+	{
+		if (!get_next_line(fd, &str) || ft_strlen(str) != 4)
+			return (0);
+		k = -1;
+		while (str[++k])
+			if (str[k] == '#')
+				if (origin.x == -1)
+					set_pos(&origin, k, j);
+				else
+				{
+					set_pos(&temp, k - origin.x, j - origin.y);
+					ft_lstaddback(&((*tetriminos)[nb].blocks),
+						ft_lstnew(&temp, sizeof(t_tetrimino)));
+				}
+	}
+}
+
 int	parse_tetriminos(t_tetrimino **tetriminos, int fd)
 {
 	char	*str;
-	size_t	i;
-	size_t	j;
-	size_t	k;
-	t_pos	origin;
-	t_pos	temp;
-	
-	i = 0;
-	while (i < 26)
+	ssize_t	i;
+
+	i = -1;
+	while (++i < 26)
 	{
-		origin.x = -1;
-		// printf("i: %zu\n", i);
-		j = 0;
-		while (j < 4)
-		{
-			// printf("j: %zu\n", j);
-			if (!get_next_line(fd, &str) || ft_strlen(str) != 4)
-				return (0);
-			fprintf(stderr, "str: %s\n", str);
-			k = 0;
-			while (str[k])
-			{
-				if (str[k] == '#')
-					if (origin.x == -1)
-					{
-						set_pos(&origin, k, j);
-						fprintf(stderr, "origin x: %d\norigin y: %d\n", origin.x, origin.y);
-					}
-					else
-					{
-						set_pos(&temp, k - origin.x, j - origin.y);
-						ft_lstaddback(&((*tetriminos)[i].blocks), ft_lstnew(&temp, sizeof(t_tetrimino)));
-						fprintf(stderr, "list x: %d\nlist y: %d\n", temp.x, temp.y);
-					}
-				k++;
-			}
-			j++;
-		}
+		parse_one_tetrimino(fd, tetriminos, i);
 		get_next_line(fd, &str);
 		if (ft_strlen(str) != 0)
 			return (0);
-		i++;
 	}
 	return (1);
 }
